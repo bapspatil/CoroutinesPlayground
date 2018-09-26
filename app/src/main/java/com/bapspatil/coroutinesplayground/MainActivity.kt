13b9d7efeb7d2499791a634a821650cfa164a4f3
Bapusaheb Patil <bapoo1998@gmail.com>
2018-09-26 12:38:51 +0530

    Added async/await for concurrent tasks

--- app/src/main/java/com/bapspatil/coroutinesplayground/MainActivity.kt
+++ app/src/main/java/com/bapspatil/coroutinesplayground/MainActivity.kt
@@ -2,11 +2,41 @@ package com.bapspatil.coroutinesplayground
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import org.jetbrains.anko.design.indefiniteSnackbar
+import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnAsyncTwoTasks.onClick { _ ->
+            val taskOneResult = async { longTaskOne() }
+            val taskTwoResult = async { longTaskTwo() }
+            linearParentLayout.indefiniteSnackbar("Result of Task 1=${taskOneResult.await()}; Task 2=${taskTwoResult.await()}", "OK") {
+                // Empty coroutine
+            }
+        }
+    }
+
+    private suspend fun longTaskOne(): Int {
+        delay(2000)
+        runOnUiThread {
+            toast("Task 1 completed.")
+        }
+        return 10
+    }
+
+    private suspend fun longTaskTwo(): Int {
+        delay(6000)
+        runOnUiThread {
+            toast("Task 2 completed.")
+        }
+        return 20
     }
 }
